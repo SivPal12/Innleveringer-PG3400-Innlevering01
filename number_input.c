1917c3d5d8559cd7985fab16abcea29788556dfe
Pal M Sivertsen <palmarius@hotmail.com>
2014-09-28 21:29:36 +0200

    implemented read of integers from file to dynamic array

--- number_input.c
+++ number_input.c
@@ -1,6 +1,52 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main (int argc, char **argv) {
-  if (argc <= 1)
+void readIntsFromFile(char filename[], int *store);
+
+int *pIntegers, integersCount;
+int dynamicArraySizeModifier = 10;
+
+int main (int argc, char *argv[]) {
+  if (argc <= 1) {
     printf("Missing file name argument!");
+    return -1;
+  }
+  readIntsFromFile(argv[1], pIntegers);
+
+  printf("%d", pIntegers[0]);
+  for (int i= 1; i < integersCount; i++) {
+    printf(" %d", pIntegers[i]);
+  }
+}
+
+void readIntsFromFile(char filename[], int *store) {
+  integersCount = 0;
+  FILE* file = fopen (filename, "r");
+  int i = 0, size = dynamicArraySizeModifier;
+  if(NULL == (pIntegers = calloc(size, sizeof(int)))){
+    printf("calloc failed");
+    return;
+  }
+
+  while (fscanf (file, "%d", &i) != EOF) {
+    integersCount++;
+    // Need to expand pIntegers?
+    if (size <= integersCount) {
+      size += dynamicArraySizeModifier; // Expand memory of 'pIntegers' by 'dynamicArraySizeModifier'
+      if (NULL == (pIntegers = realloc(pIntegers, size*sizeof(int)))) {
+        printf("realloc failed\n");
+        return;
+      }
+    }
+    pIntegers[integersCount - 1] = i;
+  }
+  fclose (file);
+
+  // Make pIntegers as small as possible
+  if (size != integersCount) {
+    if (NULL == (pIntegers = realloc(pIntegers, integersCount*sizeof(int)))) {
+      printf("realloc failed\n");
+      return;
+    }
+  }
 }
